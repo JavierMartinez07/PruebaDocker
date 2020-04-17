680b27b66675d40cbf4839253032e7be42deac75
JavierCrack <ismael.dt@hotmail.com>
2020-04-16 21:16:36 -0400

    Add project files.

---
+++ Models/CrudClientContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PruebaDocker.Models
+{
+    public partial class CrudClientContext : DbContext
+    {
+        public CrudClientContext()
+        {
+        }
+
+        public CrudClientContext(DbContextOptions<CrudClientContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("UserID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PruebaDocker.Models
+{
+    public partial class Usuarios
+    {
+        [ScaffoldColumn(false)]
+        public int UserId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public bool Activo { get; set; }
+    }
+}
